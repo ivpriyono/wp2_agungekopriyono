6cd2b7f49e0d5d2bd41d7e31e29a3de429b894b1
ivpriyono <iv.priyono@gmail.com>
2024-03-17 15:38:15 +0700

    First commit

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,13 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan<h1>";
+        echo "Nama saya Agung Eko Priyono<br>
+                 Saya tinggal di Serpong<br>
+                 suka ngulih hal-hal baru";
+    }
+}
+
+?>
