b2f0f84aebcc00779f88c0594aa9802aa83bdaf9
ivpriyono <iv.priyono@gmail.com>
2024-03-18 20:49:58 +0700

    meeting 4

---
+++ application/views/v_about.php
@@ -0,0 +1,22 @@
+<section>
+    <h1><?= $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Agung</li>
+        <li>Nama Belakang : Priyono</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Jalan Jendral Sudirman Kav 52-53</li>
+    </ul>
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Pacitam</li>
+    </ul>
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Bulutangkis</li>
+        <li>Catur</li>
+    </ul>
+</section>
