64b7c6c76ab5ea9cd444b0f27376d0259d8b8cb4
ivpriyono <iv.priyono@gmail.com>
2024-03-20 18:30:54 +0700

    Meeting 5

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined("BASEPATH") or exit("No direct script access allowed");
 | a PHP script and you can easily do that on your own.
 |
 */
-$config["base_url"] = "http://pustaka-booking.test/";
+$config["base_url"] = "http://pustaka.local/";
 
 /*
 |--------------------------------------------------------------------------
